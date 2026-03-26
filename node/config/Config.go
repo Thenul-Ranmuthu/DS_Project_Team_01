a223a8bed22b0a47edebb4e7553e2f40c5ef9a72
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-03-26 10:42:23 +0530

    Added endpoints to showcase leader status and node IDs.

---
+++ node/config/Config.go
@@ -0,0 +1,18 @@
+package config
+
+import "os"
+
+type Config struct {
+	NodeID    string
+	Port      string
+	ZKServers []string
+	Peers     []string
+}
+
+func Load() Config {
+	return Config{
+		NodeID:    os.Getenv("NODE_ID"),          // e.g. "node-5050"
+		Port:      os.Getenv("PORT"),             // e.g. "5050"
+		ZKServers: []string{"172.30.112.1:8080"}, // same for all nodes
+	}
+}
